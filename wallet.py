0ca64d04113916bde8fc5ecc3b015c0fbdf9c905
Serhat Gemici <serhatgemici@msn.com>
2026-07-26 15:50:23 +0000

    add: implement Wallet class with cash management and corresponding tests

---
+++ wallet.py
@@ -0,0 +1,15 @@
+class Wallet:
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def add_cash(self, amount):
+        self.balance += amount
+
+
+    def spend_cash(self, amount):
+        if amount > self.balance:
+            raise InsufficientAmount('Not enough available to spend {}'.format(amount))
+        self.balance -= amount
+
+class InsufficientAmount(Exception):
+    pass
